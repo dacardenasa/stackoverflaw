b877eff7ec7fc47b814ae838fe0d2c1e3f6754cb
dacardenasa <diego.cardenasaleg@gmail.com>
2020-04-20 14:35:51 -0500

    Add partials errors and last styles  css  to project

--- app/controllers/comment_answers_controller.rb
+++ app/controllers/comment_answers_controller.rb
@@ -4,14 +4,16 @@ class CommentAnswersController < ApplicationController
     @answers = Answer.where(id: params[:id]).take
     @comment_answer = @answers.CommentAnswers.create(params_comment_answers)
     @pregunta = @answers.preguntum
-    @comments = @pregunta.comments.order(created_at: :desc)
-    @answers_questions = @pregunta.answers.order(created_at: :desc)
-    @comment = Comment.new
-    @answer = Answer.new
 
     if @comment_answer.save
       redirect_to preguntum_path(@pregunta), notice: "Comentario agregado con exito"
     else
+      # Instance vars if failed
+      @comment = Comment.new
+      @answer = Answer.new
+      @comments = @pregunta.comments.order(created_at: :desc)
+      @answers_questions = @pregunta.answers.order(created_at: :desc)
+      @errores = @comment_answer.errors.full_messages
       render 'pregunta/show'
     end
 
