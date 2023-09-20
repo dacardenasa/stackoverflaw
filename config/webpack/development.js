cb985928d2f39c22134b805f5072dad6650ae68e
Diego Cardenas <diego.cardenasaleg@gmail.com>
2023-09-20 01:14:27 -0500

    updated modules import

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+import environment from './environment'
 
-const environment = require('./environment')
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 module.exports = environment.toWebpackConfig()
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,4 @@
+import environment from './environment'
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-
-const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
